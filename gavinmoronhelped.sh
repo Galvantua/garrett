66ef4e75b304651daa30f710b4c311852e31c6ec
Galvantua <galvantua@gmail.com>
2019-02-10 14:26:35 -0500

    :)

--- gavinmoronhelped.sh
+++ gavinmoronhelped.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 tree
 echo "you are a moron you big idiot and rest in peices :) -gavin moron"
-echo "can you put up tree so i can leaf you alone you idiot :) -gavin morgarrett acraway seid that you are a bit idiot are you lising you idiot hu hu:) -gavinmoron
+echo "can you put up tree so i can leaf you alone you idiot :) -gavin morgarrett acraway seid that you are a bit idiot are you lising you idiot hu hu :) -gavin moron"
+echo "-\('')/-  :) -gavinmoron"
